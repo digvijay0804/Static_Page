1214f8d1e420578dd3872f93945ad6cfd928ed48
digvijay0804 <digvijaythorat855@gmail.com>
2025-12-31 16:56:54 +0000

    first commit

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,22 +1,15 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { describe, test, expect } from 'vitest'
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import { test, expect } from 'vitest'
+import App from './App'
 
-test('renders Github Actions heading', () => {
-  render(<App />);
-  const headingElement = screen.getByText(/Github Actions/i);
-  expect(headingElement).toBeInTheDocument();
-});
+test('renders h1 heading with name', () => {
+  render(<App />)
+  const headingElement = screen.getByRole('heading', { level: 1 })
+  expect(headingElement).toHaveTextContent(/Pranav/i)
+})
 
-test('contains an h1 element', () => {
-  const { container } = render(<App />);
-  const h1Element = container.querySelector('h1');
-  expect(h1Element).toBeInTheDocument();
-});
-
-test('contains an h2 element', () => {
-  const { container } = render(<App />);
-  const h2Element = container.querySelector('h2');
-  expect(h2Element).toBeInTheDocument();
-});
+test('renders h2 description', () => {
+  render(<App />)
+  const subHeading = screen.getByText(/lovely Brother/i)
+  expect(subHeading).toBeInTheDocument()
+})
